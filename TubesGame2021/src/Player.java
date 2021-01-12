6dd439af56e544b1329236a65237e94dbd6a64ec
RoidAkbar <63292984+RoidAkbar@users.noreply.github.com>
2021-01-12 23:13:53 +0800

    interface

--- TubesGame2021/src/Player.java
+++ TubesGame2021/src/Player.java
@@ -1,13 +1,37 @@
-public class Player {
-    private String class;
+public class Player implements Attacking, Defending, Intractable, Escaping{
+    private String classPlayer;
     private String weapon;
 
-    @Override
-    public String getClass() {
-        return class;
+    public Player(String classPlayer, String weapon) {
+        this.classPlayer = classPlayer;
+        this.weapon = weapon;
+    }
+
+    public String getClassPlayer() {
+        return classPlayer;
     }
 
     public String getWeapon() {
         return weapon;
+    }
+
+    @Override
+    public void attack() {
+
+    }
+
+    @Override
+    public void defend() {
+
+    }
+
+    @Override
+    public void escape() {
+
+    }
+
+    @Override
+    public void speak() {
+
     }
 }
